ae9cff150e15bd904fd3cd98cc4b99896b788762
pedrohenriquedeveloper <115297797+pedrohenriquedeveloper@users.noreply.github.com>
2023-02-02 21:25:21 -0300

    Add files via upload

---
+++ aula-16-11-22/js/meu_script.js
@@ -0,0 +1,5 @@
+// alert("Olá alunos");
+document.getElementById("nome").value= "Mengoooo";
+
+var nome = "Ramon";
+var Nome = "Peps"; 
---
+++ aula-16-11-22/js/variaveis.js
@@ -0,0 +1,12 @@
+// scring
+var texto = "Java Script";
+// number
+var numeroInteiro = -10;
+var numeroFracionado = 10.45;
+// boolean
+var teste = false; //true -> verdadeiro
+
+// alert(numeroInteiro)
+// document.write(numeroFracionado)
+console.log(texto);
+
